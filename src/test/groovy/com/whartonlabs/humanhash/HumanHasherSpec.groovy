99c6f51d78d86f787061902c57a91f36588ca681
andrewwharton <andrew@whartonlabs.com>
2014-03-11 22:37:21 +1100

    added test for the custom word list constructor

--- src/test/groovy/com/whartonlabs/humanhash/HumanHasherSpec.groovy
+++ src/test/groovy/com/whartonlabs/humanhash/HumanHasherSpec.groovy
@@ -5,24 +5,42 @@ class HumanHasherSpec extends spock.lang.Specification {
     def "Test the compression function"() {
 
         given:
-        def humanhasher = new HumanHasher()
+        def hasher = new HumanHasher()
 
         expect:
-        humanhasher.compress([34,5,3,2,5,6,8,3,4,7,2,4,5], 3) == [38, 8, 0]
+        hasher.compress([34,5,3,2,5,6,8,3,4,7,2,4,5], 3) == [38, 8, 0]
 
-        humanhasher.compress([93, 130, 244, 167, 180, 79, 105, 3], 6) == [93, 130, 244, 167, 180, 37]
+        and:
+        hasher.compress([93, 130, 244, 167, 180, 79, 105, 3], 6) == [93, 130, 244, 167, 180, 37]
 
     }
 
     def "Test the humanize function"() {
 
         given:
-        def humanhasher = new HumanHasher()
+        def hasher = new HumanHasher()
         def digest = "7528880a986c40e78c38115e640da2a1"
 
         expect:
-        humanhasher.humanize(digest) == "three-georgia-xray-jig"
-        humanhasher.humanize(digest, 6) == "high-mango-white-oregon-purple-charlie"
+        hasher.humanize(digest) == "three-georgia-xray-jig"
+        hasher.humanize(digest, 6) == "high-mango-white-oregon-purple-charlie"
+
+    }
+
+    def "Test the custom word list constructor"() {
+
+        given: "a custom word list"
+        def words = (0..255).collect { "alfa" }
+
+        and:
+        def digest = "7528880a986c40e78c38115e640da2a1"
+
+        when:
+        def hasher = new HumanHasher(words)
+
+        then: "the human hash to contain word from the custom list"
+        hasher.humanize(digest) == "alfa-alfa-alfa-alfa"
+
 
     }
 
