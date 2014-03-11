b1209f9c28c2d06ad3d97e3af3d9a52c0c1cb352
andrewwharton <andrew@whartonlabs.com>
2014-03-12 00:16:06 +1100

    refactored and added some new tests for overloaded "humanize" method calls

--- src/test/groovy/com/whartonlabs/humanhash/HumanHasherSpec.groovy
+++ src/test/groovy/com/whartonlabs/humanhash/HumanHasherSpec.groovy
@@ -2,16 +2,19 @@ package com.whartonlabs.humanhash
 
 class HumanHasherSpec extends spock.lang.Specification {
 
-    def "Test the compression function"() {
+    def "Compression function work compress or expand byte array as specified"() {
 
-        given:
-        def hasher = new HumanHasher()
+        expect: "Same number of bytes does not modify array"
+        new HumanHasher().compress([93, 130, 244, 167, 180, 79], 6) == [93, 130, 244, 167, 180, 79]
 
-        expect:
-        hasher.compress([34,5,3,2,5,6,8,3,4,7,2,4,5], 3) == [38, 8, 0]
+        and: "One extra byte reduces correctly"
+        new HumanHasher().compress([93, 130, 244, 167, 180, 79, 105], 6) == [93, 130, 244, 167, 180, 38]
 
-        and:
-        hasher.compress([93, 130, 244, 167, 180, 79, 105, 3], 6) == [93, 130, 244, 167, 180, 37]
+        and: "More than one extra bytes reduces correctly"
+        new HumanHasher().compress([93, 130, 244, 167, 180, 79, 105, 3], 6) == [93, 130, 244, 167, 180, 37]
+
+        and: "Less number of bytes are padded with 0 bytes"
+        new HumanHasher().compress([93, 130, 244, 167, 180, 79], 9) == [93, 130, 244, 167, 180, 79, 0, 0, 0]
 
     }
 
@@ -23,8 +26,27 @@ class HumanHasherSpec extends spock.lang.Specification {
 
         expect:
         hasher.humanize(digest) == "three-georgia-xray-jig"
+
+    }
+
+    def "Test the custom length humanize function"() {
+
+        given:
+        def hasher = new HumanHasher()
+        def digest = "7528880a986c40e78c38115e640da2a1"
+
+        expect:
         hasher.humanize(digest, 6) == "high-mango-white-oregon-purple-charlie"
 
+    }
+
+    def "Test the custom length and custom separator humanize function"() {
+        given:
+        def hasher = new HumanHasher()
+        def digest = "7528880a986c40e78c38115e640da2a1"
+
+        expect:
+        hasher.humanize(digest, 6, "_") == "high_mango_white_oregon_purple_charlie"
     }
 
     def "Test the custom word list constructor"() {
