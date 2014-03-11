9e436fea5ce65426f736e4afcab51fb75364b764
andrewwharton <andrew@whartonlabs.com>
2014-03-11 22:51:34 +1100

    removed commented out uuid code

--- src/main/groovy/com/whartonlabs/humanhash/HumanHasher.groovy
+++ src/main/groovy/com/whartonlabs/humanhash/HumanHasher.groovy
@@ -116,13 +116,5 @@ class HumanHasher {
         return compressedSegments
     }
 
-//    def uuid(numWords, separator) {
-//        digest = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c){
-//            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-//            return v.toString(16);
-//        });
-//        return [this.humanize(digest, numWords, separator), digest];
-//    }
-
 }
 
